62458925e84145b15e4fe27bdd471cd4728de825
Kilril312 <kirill2016kiselev@list.ru>
2025-06-12 16:53:05 +0300

    open api

--- internal/handlers/taskHandlers.go
+++ internal/handlers/taskHandlers.go
@@ -2,59 +2,12 @@ package handlers
 
 import (
 	"domashka/internal/tasksService"
-	"domashka/internal/web/tasks"
 	"github.com/labstack/echo/v4"
-	"golang.org/x/net/context"
 	"net/http"
 )
 
 type TaskHandler struct {
 	service tasksService.TaskService
-}
-
-func (h *TaskHandler) GetTasks(ctx context.Context, request tasks.GetTasksRequestObject) (tasks.GetTasksResponseObject, error) {
-	// Получение всех задач из сервиса
-	allTasks, err := h.service.GetAllTasks()
-	if err != nil {
-		return nil, err
-	}
-
-	// Создаем переменную респон типа 200джейсонРеспонс
-	// Которую мы потом передадим в качестве ответа
-	response := tasks.GetTasks200JSONResponse{}
-
-	// Заполняем слайс response всеми задачами из БД
-	for _, tsk := range allTasks {
-		task := tasks.RequestBodyTask{
-			Id:   &tsk.ID,
-			Task: &tsk.Task,
-		}
-		response = append(response, task)
-	}
-
-	// САМОЕ ПРЕКРАСНОЕ. Возвращаем просто респонс и nil!
-	return response, nil
-}
-
-func (h *TaskHandler) PostTasks(ctx context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
-	// Распаковываем тело запроса напрямую, без декодера!
-	taskRequest := request.Body
-	// Обращаемся к сервису и создаем задачу
-	taskToCreate := tasksService.RequestBodyTask{
-		Task: *taskRequest.Task,
-	}
-	createdTask, err := h.service.CreateTask(taskToCreate)
-
-	if err != nil {
-		return nil, err
-	}
-	// создаем структуру респонс
-	response := tasks.PostTasks201JSONResponse{
-		Id:   &createdTask.ID,
-		Task: &createdTask.Task,
-	}
-	// Просто возвращаем респонс!
-	return response, nil
 }
 
 func NewTaskHandler(s tasksService.TaskService) *TaskHandler {
